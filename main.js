a22a51151124a2908a7d2cda914ec668395a084d
ChrisGrant1024 <chrisgrant1024@gmail.com>
2015-12-04 20:02:17 +1100

    big things are happening...
    you can't stop me...

---
+++ main.js
@@ -0,0 +1,56 @@
+﻿var canvas = $("canvas").get(0);
+var ctx = canvas.getContext("2d");
+
+var WIDTH = 800,
+    HEIGHT = 600,
+    FPS = 60,
+    	updates = 0;
+
+var col = {
+	black: "#000"
+};
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function init() {
+    loadAssets();
+}
+
+function loadAssets() {
+    start();
+}
+
+function start() {
+    create();
+}
+
+function create() {
+    //Creating The Player
+    p = new Player(50, 76, 64, 64, "blue");
+    p.create();
+    loop();
+}
+function loop() {
+	setTimeout(function () {
+		requestAnimationFrame(loop);
+		update();
+		render();
+	}, 1000 / FPS);
+}
+
+function update() {
+	p.update();
+}
+
+
+function render() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = col.black;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	p.render();
+}
+
+init();
---
+++ player.js
@@ -0,0 +1,83 @@
+﻿//Player
+var Player = function (x, y, w, h, c) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.c = c;
+
+	this.getX = function () {
+		return this.x;
+	}
+
+	this.getY = function () {
+		return this.y;
+	}
+
+	this.setX = function (x) {
+		this.x = x;
+	}
+
+	this.setY = function (y) {
+		this.y = y;
+	}
+
+	this.updatePosition = function (x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+Player.prototype.create = function () {
+	ctx.fillStyle = this.c;
+	ctx.fillRect(this.x, this.y, this.w, this.h);
+}
+
+var Key = {
+	_pressed: {},
+
+	LEFT: 65, //A
+	UP: 87, //W
+	RIGHT: 68, //D
+	DOWN: 83, //S
+
+	isDown: function (keyCode) {
+		return this._pressed[keyCode];
+	},
+
+	onKeyDown: function (event) {
+		this._pressed[event.keyCode] = true;
+	},
+
+	onKeyUp: function (event) {
+		delete this._pressed[event.keyCode];
+	}
+};
+
+window.addEventListener('keyup', function (event) { Key.onKeyUp(event); }, false);
+window.addEventListener('keydown', function (event) { Key.onKeyDown(event); }, false);
+
+Player.prototype.update = function () {
+	this.speed = 6;
+
+	if (Key.isDown(Key.UP)) {
+		this.y -= this.speed;
+	}
+
+	if (Key.isDown(Key.RIGHT)) {
+		this.x += this.speed;
+	}
+
+	if (Key.isDown(Key.DOWN)) {
+		this.y += this.speed;
+	}
+
+	if (Key.isDown(Key.LEFT)) {
+		this.x -= this.speed;
+	}
+}
+
+Player.prototype.render = function () {
+	ctx.fillStyle = this.c;
+	ctx.fillRect(this.x, this.y, this.w, this.h);
+}
